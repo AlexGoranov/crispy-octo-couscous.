10078d9969ad70735f4eef47b4b6a653f48581a7
Alex <aleksgoranov155@gmail.com>
2017-05-04 12:42:29 +0200

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,16 @@
-window.onload = function() {
-  var el = document.getElementsByClassName("nav")[0];
+  var icons = new Skycons(),
+          list  = [
+            "clear-day", "clear-night", "partly-cloudy-day",
+            "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
+            "fog"
+          ],
+          i,
+          icon = "clear-night";
 
-}
+          icons.set(icon, icon);
+          icons.play();
+
+      for(i = list.length; i--; )
+        icons.set(list[i], list[i]);
+
+        icons.play();
