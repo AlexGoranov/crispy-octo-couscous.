bc33ba48f05b3bcb6cb48cfedd4792f7b1c04410
Alex <aleksgoranov155@gmail.com>
2017-05-04 12:40:42 +0200

    Create custom.js

---
+++ javascript/custom.js
@@ -0,0 +1,18 @@
+<script>
+      var icons = new Skycons(),
+          list  = [
+            "clear-day", "clear-night", "partly-cloudy-day",
+            "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
+            "fog"
+          ],
+          i,
+          icon = "clear-night";
+
+          icons.set(icon, icon);
+          icons.play();
+
+      for(i = list.length; i--; )
+        icons.set(list[i], list[i]);
+
+        icons.play();
+    </script>
